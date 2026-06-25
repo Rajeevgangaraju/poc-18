5ef9456e347634ea0ae71439c76d130f4a0f8c6a
Rajeevgangaraju <134952680+Rajeevgangaraju@users.noreply.github.com>
2026-06-25 11:16:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "pavan-poc18-state"
+    bucket         = "rajeev-poc-18"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "pavan-lock"
+    dynamodb_table = "rajeev-pov18lock"
   }
 }
